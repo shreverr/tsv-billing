fa01c0bb9ef3e231af409db6c13aa03e11e45f0c
Shreshth Verma <verma2007s@gmail.com>
2023-11-30 17:26:24 +0530

    pdf generation completed

--- server/controller/invoiceController.ts
+++ server/controller/invoiceController.ts
@@ -2,27 +2,30 @@ import puppeteer from 'puppeteer'
 import path from 'path'
 import ejs from 'ejs'
 import { Request, RequestHandler, Response } from 'express'
+import { nanoid } from 'nanoid'
 
 export const generateInvoice: RequestHandler = async (req: Request, res: Response) => {
   try {
     let totalAmount = 0
     req.body.tableData.forEach((row: any) => {
-      console.log(row.amount);
-      
-      console.log(Number(row.amount));
-      
-      totalAmount += Number(row.amount)
+      row.netAmount = Number(row.amount) * Number(row.quantity)
+      totalAmount += row.netAmount
+    })
 
-    });
+    const invoiceNumber = nanoid(10)
 
-    console.log(totalAmount);
+    const data = {
+      ...req.body,
+      totalAmount: totalAmount,
+      invoiceNumber: invoiceNumber,
+    }
     
     const invoiceTemplatePath = (path.dirname(__dirname) + '/views/templates/pdf-template.ejs').split(path.sep).join(path.posix.sep)
     const browser = await puppeteer.launch({
       headless: 'new'
     });
     const page = await browser.newPage();
-    await page.setContent(await ejs.renderFile(invoiceTemplatePath))
+    await page.setContent(await ejs.renderFile(invoiceTemplatePath, { ...data }))
     await page.pdf({ path: 'pdf/example.pdf', format: 'A4', printBackground: true });
     await browser.close();
 
