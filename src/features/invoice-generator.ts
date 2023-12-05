54c8eea9a553e68a5328ead0738c133ac0cc290a
Shreshth Verma <verma2007s@gmail.com>
2023-12-05 13:33:58 +0530

    try 1

--- src/features/invoice-generator.ts
+++ src/features/invoice-generator.ts
@@ -15,7 +15,7 @@ const generateInvoice = async (data: any) => {
     const browser = await puppeteer.launch({
       args: chromium.args,
       // defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
     });
     
