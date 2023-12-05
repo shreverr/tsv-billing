943b270ec9b39f5fd281815ac35d83384e877009
Shreshth Verma <verma2007s@gmail.com>
2023-12-05 12:50:48 +0530

    used another chrome module instead if alc

--- src/features/invoice-generator.ts
+++ src/features/invoice-generator.ts
@@ -1,22 +1,32 @@
 import path from "path"
-import puppeteer from "puppeteer";
+// import puppeteer from "puppeteer";
 import ejs from 'ejs'
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 const generateInvoice = async (data: any) => {
   try {
     console.log(process.cwd());
-    
+
     const invoiceTemplatePath = (
       process.cwd() + '/public/views/templates/pdf-template.ejs'
-      ).split(path.sep).join(path.posix.sep)
-  
-      const browser = await puppeteer.launch({
-        headless: 'new'
-      });
-      const page = await browser.newPage();
-      await page.setContent(await ejs.renderFile(invoiceTemplatePath, { ...data }))
-      await page.pdf({ path: 'pdf/example.pdf', format: 'A4', printBackground: true });
-      await browser.close();
+    ).split(path.sep).join(path.posix.sep)
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+    
+    //use this browser for local development comment the top one
+    // const browser = await puppeteer.launch({
+    //   headless: 'new'
+    // });
+
+    const page = await browser.newPage();
+    await page.setContent(await ejs.renderFile(invoiceTemplatePath, { ...data }))
+    await page.pdf({ path: 'pdf/example.pdf', format: 'A4', printBackground: true });
+    await browser.close();
   } catch (error) {
     console.log(`error at generating file`, error);
   }
